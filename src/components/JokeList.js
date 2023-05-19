c08cfa142b9be56ce0167eba3517e18872b0b33e
SvetaLantsman <svetlana@curalife.com>
2023-05-19 21:39:03 +0300

    .

--- src/components/JokeList.js
+++ src/components/JokeList.js
@@ -1,16 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 const JokeList = ({ jokes, onUpdateJoke, onDeleteJoke }) => {
+  const [updatedJoke, setUpdatedJoke] = useState({ id: "", type: "", setup: "", punchline: "" });
+
+  const handleUpdateJoke = () => {
+    onUpdateJoke(updatedJoke.id, updatedJoke);
+    setUpdatedJoke({ id: "", type: "", setup: "", punchline: "" });
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUpdatedJoke((prevJoke) => ({
+      ...prevJoke,
+      [name]: value,
+    }));
+  };
+
   return (
     <div>
       {jokes.map((joke) => (
         <div key={joke.id}>
           <h3>{joke.setup}</h3>
           <p>{joke.punchline}</p>
-          <button onClick={() => onUpdateJoke(joke.id, { ...joke, type: "updated" })}>
-            Update
-          </button>
-          <div><button onClick={() => onDeleteJoke(joke.id)}>Delete</button></div>
+          {updatedJoke.id === joke.id ? (
+            <div>
+              <input
+                type="text"
+                name="type"
+                value={updatedJoke.type}
+                placeholder="Type"
+                onChange={handleInputChange}
+              />
+              <input
+                type="text"
+                name="setup"
+                value={updatedJoke.setup}
+                placeholder="Setup"
+                onChange={handleInputChange}
+              />
+              <input
+                type="text"
+                name="punchline"
+                value={updatedJoke.punchline}
+                placeholder="Punchline"
+                onChange={handleInputChange}
+              />
+              <button onClick={handleUpdateJoke}>Save</button>
+            </div>
+          ) : (
+            <div>
+              <button onClick={() => setUpdatedJoke(joke)}>Update Joke</button> 
+              <div> <button onClick={() => onDeleteJoke(joke.id)}>Delete Joke</button></div>           
+            </div>
+           
+          )}
         </div>
       ))}
     </div>
